e32b5f7328841525cc218a9819c01cf40b8b75be
JungWook <78420639+stoneKR@users.noreply.github.com>
2021-03-22 11:09:58 +0900

    Add files via upload

--- Test/Test28/main/java/com/example/test28/SubActivity.java
+++ Test/Test28/main/java/com/example/test28/SubActivity.java
@@ -1,6 +1,10 @@
 package com.example.test28;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -12,8 +16,19 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        Button Btn2 = findViewById(R.id.Btn2);
+        Btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("로그","값");
+            }
+        });
+
     }
-    public void Btn1Click(){
-        Toast.makeText(this, "ㅋㅋ", Toast.LENGTH_SHORT).show();
+    public void Btn1Click(View v){
+        Toast toast = Toast.makeText(this, "ㅋㅋ", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.END | Gravity.BOTTOM, 10, 10);
+        toast.show();
     }
 }
