ed1c71c86fb158d5e99623995b8893c0bc142199
JungWook <78420639+stoneKR@users.noreply.github.com>
2021-03-30 15:19:59 +0900

    Add files via upload

---
+++ Gift/main/java/com/example/gift/Home.java
@@ -0,0 +1,16 @@
+package com.example.gift;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Home {
+
+    Button Home = findViewById(R.id.Home);
+    Home.setOnClickListener(new View.OnClickListener(){
+        @Override
+                public Void onClick(View view){
+            Log.d("로그", "값")
+        }
+    })
+}
