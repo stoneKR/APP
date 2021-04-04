890324a3f72a56080abbee3755ca28308320300a
JungWook <78420639+stoneKR@users.noreply.github.com>
2021-04-04 15:55:51 +0900

    Add files via upload

--- Function/main/java/com/example/function/MainActivity.kt
+++ Function/main/java/com/example/function/MainActivity.kt
@@ -17,30 +17,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        viewList.add(layoutInflater.inflate(R.layout.fragment_board, null))
         viewList.add(layoutInflater.inflate(R.layout.fragment_home, null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_homeworship, null))
         viewList.add(layoutInflater.inflate(R.layout.fragment_bible, null))
         viewList.add(layoutInflater.inflate(R.layout.fragment_hymn, null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_notification, null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_board, null))
 
         viewPager.adapter = pagerAdepter()
 
         viewPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener(){
             override fun onPageSelected(position: Int) {
                 when(position) {
-                    0 -> bottomNavigationView.selectedItemId = R.id.board
-                    1 -> bottomNavigationView.selectedItemId = R.id.home
+                    0 -> bottomNavigationView.selectedItemId = R.id.home
+                    1 -> bottomNavigationView.selectedItemId = R.id.homeworship
                     2 -> bottomNavigationView.selectedItemId = R.id.bible
                     3 -> bottomNavigationView.selectedItemId = R.id.hymn
+                    4 -> bottomNavigationView.selectedItemId = R.id.notification
+                    5 -> bottomNavigationView.selectedItemId = R.id.board
                 }
             }
         })
 
         bottomNavigationView.setOnNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.board -> viewPager.setCurrentItem(0)
-                R.id.home -> viewPager.setCurrentItem(1)
+                R.id.home -> viewPager.setCurrentItem(0)
+                R.id.homeworship -> viewPager.setCurrentItem(1)
                 R.id.bible -> viewPager.setCurrentItem(2)
                 R.id.hymn -> viewPager.setCurrentItem(3)
+                R.id.notification -> viewPager.setCurrentItem(4)
+                R.id.board -> viewPager.setCurrentItem(5)
             }
             return@setOnNavigationItemSelectedListener true
         }
