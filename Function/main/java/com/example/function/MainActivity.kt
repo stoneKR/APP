d0ef85e33ef8ba8039eb5ac7fa17aec25293a343
JungWook <78420639+stoneKR@users.noreply.github.com>
2021-04-04 15:13:55 +0900

    Add files via upload

---
+++ Function/main/java/com/example/function/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.function
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var viewList = ArrayList<View>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        viewList.add(layoutInflater.inflate(R.layout.fragment_board, null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_home, null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_bible, null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_hymn, null))
+
+        viewPager.adapter = pagerAdepter()
+
+        viewPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener(){
+            override fun onPageSelected(position: Int) {
+                when(position) {
+                    0 -> bottomNavigationView.selectedItemId = R.id.board
+                    1 -> bottomNavigationView.selectedItemId = R.id.home
+                    2 -> bottomNavigationView.selectedItemId = R.id.bible
+                    3 -> bottomNavigationView.selectedItemId = R.id.hymn
+                }
+            }
+        })
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.board -> viewPager.setCurrentItem(0)
+                R.id.home -> viewPager.setCurrentItem(1)
+                R.id.bible -> viewPager.setCurrentItem(2)
+                R.id.hymn -> viewPager.setCurrentItem(3)
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
+
+    inner class pagerAdepter : PagerAdapter() {
+        override fun isViewFromObject(view: View, `object`: Any) = view == `object`
+
+        override fun getCount() = viewList.size
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            var curView = viewList[position]
+            viewPager.addView(curView)
+            return curView
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            viewPager.removeView(`object` as View)
+        }
+
+    }
+}
